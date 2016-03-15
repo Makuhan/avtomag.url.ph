fb69a00888115dee01e1956a88505a7943fb9081
Roman Karplyak <warior201@gmail.com>
2016-03-15 23:55:54 +0200

    test

--- manga/header.php
+++ manga/header.php
@@ -66,13 +66,13 @@ if ($conn->connect_error) {
 $query = 'SELECT db_manga.id, db_manga.name, db_chapter.N_chapter,db_page.N_page, db_page.path
 FROM db_manga LEFT JOIN manga.db_chapter ON db_manga.id=db_chapter.id_manga 
 RIGHT JOIN manga.db_PAGE ON db_chapter.id=db_page.id_chapter
-where db_manga.id='.$id.'order by db_page.N_page';
+where db_manga.id='.$id.' order by db_page.N_page';
 //$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
 
-$query = 'SELECT db_manga.id, db_manga.name, db_chapter.N_chapter,db_page.N_page, db_page.path FROM db_manga 
+/*$query = 'SELECT db_manga.id, db_manga.name, db_chapter.N_chapter,db_page.N_page, db_page.path FROM db_manga 
 left join db_chapter on db_manga.id=db_chapter.id_manga
     right join db_page on db_chapter.id=db_page.id_chapter
-     WHERE db_manga.id='.$id;
+     WHERE db_manga.id='.$id;*/
 
 $result = $conn->query($query);
 
